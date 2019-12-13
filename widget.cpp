46d961cdd332e7ec56d94ce25f37bd1853a8d898
Jucov Alexandr <sania337@gmail.com>
2019-12-13 14:04:56 +0200

    .

--- widget.cpp
+++ widget.cpp
@@ -1,24 +1,30 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QDebug>
-
+#include <QNetworkProxy>
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    QNetworkAccessManager *managet = new QNetworkAccessManager(this);
-    connect(managet, &QNetworkAccessManager::finished, this, &Widget::replyFinished);
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Widget::checkSite);
+    trayIcon = new QSystemTrayIcon(this);
+    trayIcon->setIcon(QIcon(":/new/prefix1/favicon.png"));
+    trayIcon->setToolTip("RegistruMd");
+    trayIcon->show();
+
+    manager = new QNetworkAccessManager(this);
+    connect(manager, &QNetworkAccessManager::finished, this, &Widget::replyFinished);
     QNetworkRequest request;
+
     request.setUrl(QUrl("https://programari.registru.md/wp-admin/admin-ajax.php"));
-    request.setRawHeader(QByteArray("Accept"), QByteArray("*/*"));
+    request.setRawHeader(QByteArray("Accept"), QByteArray("*"));
     request.setRawHeader(QByteArray("Origin"), QByteArray("https://programari.registru.md"));
     request.setRawHeader(QByteArray("X-Requested-With"), QByteArray("XMLHttpRequest"));
     request.setRawHeader(QByteArray("Content-Type"), QByteArray("application/x-www-form-urlencoded; charset=UTF-8"));
-    managet->post(request,QByteArray("ProviderId:36"
-                                     "ServiceId:338"
-                                     "action:GetProviderAvailableDatesByService"));
-    //managet->get(QNetworkRequest(QUrl("https://programari.registru.md")));
+    manager->post(request,QByteArray("ProviderId=36&ServiceId=338&action=GetProviderAvailableDatesByService"));
+    manager->post(request,QByteArray("Date=2019-07-26&ProviderId=36&ServiceId=338&action=GetProviderAvailableHoursByDate"));
 }
 
 Widget::~Widget()
@@ -26,7 +32,63 @@ Widget::~Widget()
     delete ui;
 }
 
+
 void Widget::replyFinished(QNetworkReply *reply)
 {
-    qDebug()<<reply->readAll();
+    QByteArray bt = reply->readAll();
+    //qDebug()<<bt;
+    bt.replace('"', ' ');
+    bt.replace(']', ' ');
+    bt.replace('[', ' ');
+
+    QList<QByteArray> sp = bt.split(',');
+    for(QByteArray ar: sp){
+        ar = ar.trimmed();
+        //qDebug()<<ar;
+        QDate dt = QDate::fromString(ar, "yyyy-MM-dd");
+        //qDebug()<<dt;
+        if(dt < QDate(2020,1,15) && dt > QDate::currentDate()){
+            qDebug()<<dt;
+            trayIcon->showMessage("Warning!","Free date!!!\n"+dt.toString());
+        }
+    }
+}
+
+void Widget::checkSite()
+{
+
+   /* QEventLoop loop;
+    connect(manager, &QNetworkAccessManager::finished, &loop, &QEventLoop::quit);
+    loop.exec();*/
+    QNetworkRequest request;
+
+    request.setUrl(QUrl("https://programari.registru.md/wp-admin/admin-ajax.php"));
+    request.setRawHeader(QByteArray("Accept"), QByteArray("*"));
+    request.setRawHeader(QByteArray("Origin"), QByteArray("https://programari.registru.md"));
+    request.setRawHeader(QByteArray("X-Requested-With"), QByteArray("XMLHttpRequest"));
+    request.setRawHeader(QByteArray("Content-Type"), QByteArray("application/x-www-form-urlencoded; charset=UTF-8"));
+
+    QNetworkReply *re = manager->post(request,QByteArray("ProviderId=36&ServiceId=338&action=GetProviderAvailableDatesByService"));
+    //re->waitForReadyRead(1000);
+    //qDebug()<<re->readAll();
+    //manager->post(request,QByteArray("Date=2019-07-26&ProviderId=36&ServiceId=338&action=GetProviderAvailableHoursByDate"));
+
+}
+
+void Widget::on_pushButton_clicked()
+{
+    qDebug()<<"Timer started";
+    timer->start(1000);
+}
+
+void Widget::changeEvent(QEvent *event)
+{
+    QWidget::changeEvent(event);
+    if (event -> type() == QEvent::WindowStateChange)
+    {
+        if (isMinimized())
+        {
+            this -> hide();
+        }
+    }
 }
--- widget.h
+++ widget.h
@@ -2,8 +2,11 @@
 #define WIDGET_H
 
 #include <QWidget>
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtNetwork/QNetworkReply>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QSystemTrayIcon>
+#include <QTimer>
+
 namespace Ui {
 class Widget;
 }
@@ -12,14 +15,28 @@ class Widget : public QWidget
 {
     Q_OBJECT
 
+    struct FreeDate{
+        QDate date;
+        QTime time;
+    };
+
+
 public:
     explicit Widget(QWidget *parent = nullptr);
     ~Widget();
 
 private:
     Ui::Widget *ui;
+    QNetworkAccessManager *manager;
+    QTimer *timer;
+    QSystemTrayIcon *trayIcon;
+    void changeEvent(QEvent *event);
+
+    bool isDataReceived = 0;
 private slots:
     void replyFinished(QNetworkReply *reply);
+    void checkSite();
+    void on_pushButton_clicked();
 };
 
 #endif // WIDGET_H
